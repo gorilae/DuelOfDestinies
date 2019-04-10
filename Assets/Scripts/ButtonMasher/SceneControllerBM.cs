6ac4386ac7b9548e650ab862e0f1487f4dae4a0c
Erik Espinosa <ese26@case.edu>
2019-04-10 03:37:23 -0400

    Added ButtonMasher
    Expect WheelMasher over the weekend

---
+++ Assets/Scripts/ButtonMasher/SceneControllerBM.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneControllerBM : MonoBehaviour
+{
+    private int player1total;
+    private int player2total;
+    private int randomKey;
+    private KeyCode player1Key;
+    private KeyCode player2Key;
+
+    public int goaltotal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player1total = 0;
+        player2total = 0;
+        randomKey = Random.Range(0, 4);
+
+        //Default Key Presser
+        player1Key = KeyCode.W;
+        player2Key = KeyCode.UpArrow;
+
+        //Randomness with Button Mashed Key
+        if(randomKey==1)
+        {
+            player1Key = KeyCode.A;
+            player2Key = KeyCode.LeftArrow;
+        }
+        if (randomKey == 2)
+        {
+            player1Key = KeyCode.S;
+            player2Key = KeyCode.DownArrow;
+        }
+        if (randomKey == 3)
+        {
+            player1Key = KeyCode.D;
+            player2Key = KeyCode.RightArrow;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameOver())
+        {
+            if(Input.GetKeyDown(player1Key))
+            {
+                player1total++;
+            }
+            if(Input.GetKeyDown(player2Key))
+            {
+                player2total++;
+            }
+        }
+    }
+
+    public bool GameOver()
+    {
+        if(player1total==goaltotal || player2total==goaltotal)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public string GetPlayer1Key()
+    {
+        if(player1Key==KeyCode.W)
+        {
+            return "w";
+        }
+        else if (player1Key == KeyCode.A)
+        {
+            return "a";
+        }
+        else if (player1Key == KeyCode.S)
+        {
+            return "s";
+        }
+        else if (player1Key == KeyCode.D)
+        {
+            return "d";
+        }
+
+        //Default response
+        return "w";
+    }
+
+    public string GetPlayer2Key()
+    {
+        if (player2Key == KeyCode.UpArrow)
+        {
+            return "up";
+        }
+        else if (player2Key == KeyCode.LeftArrow)
+        {
+            return "left";
+        }
+        else if (player2Key == KeyCode.DownArrow)
+        {
+            return "down";
+        }
+        else if (player2Key == KeyCode.RightArrow)
+        {
+            return "right";
+        }
+
+        //Default response
+        return "up";
+    }
+}
+}
+}
+}
---
+++ Assets/Scripts/ButtonMasher/TextSelectorBM.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TextSelectorBM : MonoBehaviour
+{
+    public SceneControllerBM controller;
+    public int playernum;
+    public Text buttonText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playernum == 1)
+        {
+            buttonText.text = controller.GetPlayer1Key();
+        }
+        else
+        {
+            buttonText.text = controller.GetPlayer2Key();
+        }
+    }
+}
