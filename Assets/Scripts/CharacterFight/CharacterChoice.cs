18ee70aa713e07f19c4c77d0f851041e1f7c3d3e
Alex <acf63@case.edu>
2019-04-22 20:53:00 -0400

    begun character fight scenes

---
+++ Assets/Scripts/CharacterFight/CharacterChoice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterChoice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //place characters on their side of the screen
+        //access character choice through master controller
+        //place sprite on board
+        //show current score above characters
+    }
+
+}
